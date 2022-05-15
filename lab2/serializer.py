8139d3a7c783ec8e18fe71bba4b04fc171c5f0ac
krishtels <05350083@study.bsuir.by>
2022-05-15 23:05:59 +0300

    Update serializer.py

--- lab2/serializer.py
+++ lab2/serializer.py
@@ -4,10 +4,10 @@ from serializer import Factory
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Serializer of YAML, TOML, JSON')
-    parser.add_argument('input_dir', type=str, help='Input dir for videos')
+    parser.add_argument('input_dir', type=str, help='Input dir')
     parser.add_argument('source_format', type=str, help='Serialize format(json, toml or yaml) of source')
     parser.add_argument('result_format', type=str, help='Serialize format(json, toml or yaml) for result')
-    parser.add_argument('output_dir', type=str, help='Output dir for image')
+    parser.add_argument('output_dir', type=str, help='Output dir')
     args = parser.parse_args()
 
     result_format = args.result_format
