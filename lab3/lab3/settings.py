c497f896a9e11fddfc42cc9cca65079ae114401a
krishtels <05350083@study.bsuir.by>
2022-06-09 05:41:45 +0300

    Automate remote migrations

--- lab3/lab3/settings.py
+++ lab3/lab3/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddlewar',
 ]
 
 ROOT_URLCONF = 'lab3.urls'
